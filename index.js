ab4ed79b4527a4df911c75cc1fd8f60d3aba78dd
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-10-24 20:03:00 +0100

    removed a route

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ dotenv.config();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(require("./routes/route"));
-app.get("/", (req, res) => res.send("Hello World!"));
+
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`)
 );

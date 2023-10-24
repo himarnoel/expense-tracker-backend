36bfb99d26b1423ad2904db21200dc1ba6476eb4
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-10-24 19:53:29 +0100

    body parser

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: false }));
 app.use(require("./routes/route"));
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(process.env.PORT, () =>

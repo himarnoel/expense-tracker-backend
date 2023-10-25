1019344e35c1e055629bfb9153d24e08fe2adb6e
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-10-25 15:02:35 +0100

    added a new route

--- routes/route.js
+++ routes/route.js
@@ -2,7 +2,9 @@ const routes = require("express").Router();
 const {
   create_Categories,
   get_Categories,
+  create_Transaction,
 } = require("../controller/controller");
 routes.route("/api/categories").post(create_Categories).get(get_Categories);
+routes.route("/api/transaction").post(create_Transaction);
 
 module.exports = routes;

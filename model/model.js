142c897dea9e86da57dfafb90b6ec0e82f730e1f
Emmanuel Olaniyi <olaimarnoel@gmail.com>
2023-10-25 11:12:57 +0100

    done with models

--- model/model.js
+++ model/model.js
@@ -4,6 +4,22 @@ const Schema = mongoose.Schema;
 // Categories
 const categories_model = new Schema({
   type: { type: string, default: "Investment" },
- color: { type: string, default: "#FCBE44" },
+  color: { type: string, default: "#FCBE44" },
 });
 
+//transaction
+const transaction_model = new Schema({
+  name: { type: string, default: "Anonymous" },
+  type: { type: string, default: "Investment" },
+  amount: { type: Number, default: "Investment" },
+  date: { type: Date, default: Date.now() },
+});
+
+const Categories = mongoose.model("categories", categories_model);
+const Transaction = mongoose.model("transaction", transaction_model);
+
+exports.default = Transaction;
+module.exports = {
+  Categories,
+  Transaction,
+};
